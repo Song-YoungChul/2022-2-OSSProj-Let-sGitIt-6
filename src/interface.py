4986db5b0257e3bad33e806ccd8c29f026c1b1b2
MealWithoutSoup <asdr111324@gmail.com>
2022-11-01 18:01:17 +0900

    [22.11.01,김재홍 ImgBack클래스 추가]

--- src/interface.py
+++ src/interface.py
@@ -105,3 +105,33 @@ class Scoreboard:
             self.image.blit(self.tempimages[s], self.temprect)
             self.temprect.left += self.temprect.width
         self.temprect.left = 0
+
+# 이미지 배경 11/1추가
+class ImgBack:
+    def __init__(self, speed=-5, name='ground'):
+        self.image, self.rect = load_image(f'{name}.png', width, height)
+        self.image1, self.rect1 = load_image(f'{name}.png', width, height)
+        self.rect.bottom = height
+        self.rect1.bottom = height
+        self.rect1.left = self.rect.right
+        # 화면 맞추기
+        self.rect.bottom = height
+        self.rect1.bottom = height
+        self.rect.top = 0
+        self.rect1.top = 0
+        # image1 우측에 붙이기
+        self.rect.left = 0
+        self.rect1.left = self.rect.right
+        self.speed = speed
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+        screen.blit(self.image1, self.rect1)
+
+    def update(self):
+        self.rect.left += self.speed
+        self.rect1.left += self.speed
+        if self.rect.right < width:
+            self.rect1.left = self.rect.right
+        if self.rect1.right < width:
+            self.rect.left = self.rect1.right
